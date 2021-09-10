a7653e6bbb8dd0ba95b4d7747cac50d43e7ab8ef
UnturnedX <therealdeadmaster@gmail.com>
2021-09-10 19:25:01 +0100

    Prevent the boss from damaged

--- src/main/java/me/unturned/outpost/events/listeners/PreventBossKnockbackListener.java
+++ src/main/java/me/unturned/outpost/events/listeners/PreventBossKnockbackListener.java
@@ -4,15 +4,21 @@ import org.bukkit.entity.Zombie;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
-import org.bukkit.util.BlockVector;
 
-public class PreventBossKnockbackListener implements Listener {
+public class BossDamageListener implements Listener {
+
+    /*
+    * This prevents the boss from being attacked if it
+    * has only been spawned for 10 seconds
+    * */
 
     @EventHandler
     public void onEntityDamage(EntityDamageByEntityEvent event) {
         if (!(event.getEntity() instanceof Zombie zombie)) return;
         if (!(zombie.hasMetadata("boss"))) return;
 
-        zombie.setVelocity(new BlockVector(0, 0, 0));
+        if (event.getEntity().getTicksLived() < 200) {
+            event.setCancelled(true);
+        }
     }
 }
