29e850df6bfcfe244fef486f40a3f06d2292e5e9
UnturnedX <therealdeadmaster@gmail.com>
2021-09-10 19:23:48 +0100

    Finalized - handle the boss's player target

--- src/main/java/me/unturned/outpost/events/BossEvent.java
+++ src/main/java/me/unturned/outpost/events/BossEvent.java
@@ -2,14 +2,15 @@ package me.unturned.outpost.events;
 
 import me.unturned.outpost.Outpost;
 import me.unturned.outpost.objects.region.SpawnRegion;
+import me.unturned.outpost.utils.DurationUtil;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.attribute.Attribute;
 import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.Creature;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
-import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.EntityEquipment;
 import org.bukkit.inventory.ItemStack;
@@ -18,6 +19,7 @@ import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 import org.bukkit.scheduler.BukkitRunnable;
 
+import java.time.Instant;
 import java.util.List;
 
 public class BossEvent implements Event {
@@ -27,8 +29,13 @@ public class BossEvent implements Event {
     public void start() {
         final SpawnRegion region = outpost.getRegion();
         final World world = region.getWorld();
+
+        if (world == null) {
+            return;
+        }
+
         final Location midpoint = region.getLocation1().toVector().midpoint(region.getLocation2().toVector()).toLocation(world);
-        final LivingEntity boss = (LivingEntity) world.spawnEntity(midpoint, EntityType.ZOMBIE);
+        final Creature boss = (Creature) world.spawnEntity(midpoint, EntityType.ZOMBIE);
 
         boss.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 200, 10, false, false));
         boss.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, Integer.MAX_VALUE, 3));
@@ -40,7 +47,7 @@ public class BossEvent implements Event {
 
         final EntityEquipment equipment = boss.getEquipment();
         equipment.setHelmet(armour(Material.DIAMOND_HELMET));
-        equipment.setChestplate(armour(Material.DIAMOND_HELMET));
+        equipment.setChestplate(armour(Material.DIAMOND_CHESTPLATE));
         equipment.setLeggings(armour(Material.DIAMOND_LEGGINGS));
         equipment.setBoots(armour(Material.DIAMOND_BOOTS));
 
@@ -51,18 +58,54 @@ public class BossEvent implements Event {
 
         // Handle the boss's target
         new BukkitRunnable() {
+            Instant discoveredTarget = Instant.now();
+            Player target = null;
+
             @Override
             public void run() {
-                final List<Entity> nearbyPlayers = boss.getNearbyEntities(10, 0, 10);
-                nearbyPlayers.removeIf(nearbyPlayer -> !(nearbyPlayer instanceof Player));
+                if (boss.isDead()) {
+                    cancel();
+                    return;
+                }
 
+                if (target != null  && !(target.isDead())) {
+                    if (DurationUtil.getSecondsTillEnd(discoveredTarget) < 15) {
+                        return;
+                    }
+                }
+
+                final List<Entity> nearbyDiamondPlayers = boss.getNearbyEntities(10, 0, 10);
+                nearbyDiamondPlayers.removeIf(nearbyPlayer -> nearbyPlayer instanceof Player player && player.getInventory().getHelmet() != null && player.getInventory().getHelmet().getType().equals(Material.DIAMOND_HELMET));
+
+                if (nearbyDiamondPlayers.isEmpty()) {
+                    Player closestPlayer = null;
+                    double closestDistance = 100.0D;
+
+                    for (Player player : world.getPlayers()) {
+                        if (closestPlayer == null) {
+                            closestPlayer = player;
+                        }
+                        final double distance = closestPlayer.getLocation().distance(boss.getLocation());
+                        if (distance < closestDistance) {
+                            closestDistance = distance;
+                            closestPlayer = player;
+                        }
+                    }
+
+                    target = closestPlayer;
+                }
+                else {
+                    target = (Player) nearbyDiamondPlayers.get(0);
+                    discoveredTarget = discoveredTarget.plusSeconds(15);
+                }
+
+                boss.setTarget(target);
             }
-        }.runTaskTimerAsynchronously(outpost, 200, 13);
+        }.runTaskTimer(outpost, 200, 10);
     }
 
     @Override
     public void end() {
-        // TODO handle death for mini boss
 
     }
 
