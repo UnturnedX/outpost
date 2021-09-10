12f5175e62201e58c0604800ba6975da73432565
UnturnedX <therealdeadmaster@gmail.com>
2021-09-10 19:04:57 +0100

    Improved Outpost#isBoostTime

--- src/main/java/me/unturned/outpost/Outpost.java
+++ src/main/java/me/unturned/outpost/Outpost.java
@@ -5,21 +5,19 @@ import me.unturned.outpost.listeners.PlayerMobDamageListener;
 import me.unturned.outpost.listeners.PlayerMobKillListener;
 import me.unturned.outpost.objects.Mob;
 import me.unturned.outpost.objects.region.SpawnRegion;
+import me.unturned.outpost.tasks.LoadBoostsTask;
 import me.unturned.outpost.tasks.LoadMobRewardsTask;
 import me.unturned.outpost.tasks.LoadRegionTask;
 import me.unturned.outpost.tasks.MobSpawnTask;
 import me.unturned.outpost.utils.DurationUtil;
 import net.milkbowl.vault.economy.Economy;
-import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.time.Instant;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.HashMap;
-import java.util.concurrent.ThreadLocalRandom;
 import java.util.stream.Collectors;
 
 public class Outpost extends JavaPlugin {
@@ -37,8 +35,6 @@ public class Outpost extends JavaPlugin {
         instance = this;
         economy = getServer().getServicesManager().getRegistration(Economy.class).getProvider();
 
-        loadBoosts();
-
         // Commands
         getCommand("outpost").setExecutor(new OutpostCommand(this));
 
@@ -48,6 +44,7 @@ public class Outpost extends JavaPlugin {
 
         // Tasks
         new LoadMobRewardsTask(this).runTask(this);
+        new LoadBoostsTask(this).runTaskAsynchronously(this);
         new LoadRegionTask().runTaskAsynchronously(this);
 
         new MobSpawnTask(this).runTaskTimerAsynchronously(this, 40, 20);
@@ -55,44 +52,22 @@ public class Outpost extends JavaPlugin {
 
     @Override
     public void onDisable() {
-
-        // Save boost timers
         getConfig().set("boosts", scheduledBoosts.stream().map(Instant::toEpochMilli).collect(Collectors.toCollection(ArrayList::new)));
         saveConfig();
     }
 
-    public void loadBoosts() {
-        final FileConfiguration config = getConfig();
-        boolean refreshBoosts = config.get("boosts", null) == null;
-
-        for (Long boost : config.getLongList("boosts")) {
-            final Instant instant = Instant.ofEpochMilli(boost);
-
-            if (config.getLongList("boosts").isEmpty() || Date.from(instant).getDay() != Date.from(Instant.now()).getDay()) {
-                refreshBoosts = true;
-                break;
-            }
-            scheduledBoosts.add(instant);
-        }
-
-        if (refreshBoosts) {
-            for (int i = 0; i < 4; i++) {
-                // TODO finalize the time arrangement
-                scheduledBoosts.add(Instant.now().plusSeconds(ThreadLocalRandom.current().nextInt(100, 86400)));
-            }
-        }
-    }
-
     public boolean isBoostTime() {
-        Instant instant = null;
-        for (Instant scheduledBoost : scheduledBoosts) {
-            if (instant == null) {
-                instant = scheduledBoost;
-            }
-            else if (instant.isBefore(scheduledBoost)) {
-                instant = scheduledBoost;
-            }
+        if (scheduledBoosts.isEmpty()) {
+            new LoadBoostsTask(this).runTaskAsynchronously(this);
+            return false;
         }
+
+        final Instant instant = scheduledBoosts.get(0);
+
+        if (DurationUtil.isOver(instant)) {
+            scheduledBoosts.remove(instant);
+        }
+
         return DurationUtil.isOver(instant);
     }
 
@@ -106,6 +81,10 @@ public class Outpost extends JavaPlugin {
 
     public HashMap<EntityType, Integer> getRewards() {
         return rewards;
+    }
+
+    public ArrayList<Instant> getScheduledBoosts() {
+        return scheduledBoosts;
     }
 
     public Economy getEconomy() {
